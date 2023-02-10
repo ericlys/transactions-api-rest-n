af24f89b68e84828d08d7ffea73e1398e55b89ba
Ericlys Moreira <ericlysm@gmail.com>
2023-02-10 17:46:20 -0300

    feat: setup dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,27 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transactions = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transactions
+})
+
+app.get('/hello2', async () => {
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
 })
 
 app
