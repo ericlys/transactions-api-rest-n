f8ca2d8df651aadc8b1e5c8a7a28ff51342bcc75
Ericlys Moreira <ericlysm@gmail.com>
2023-02-10 19:36:20 -0300

    feat: create transaction route

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,11 @@
 import fastify from 'fastify'
-import { knex } from './database'
-import crypto from 'node:crypto'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transactions = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação teste',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transactions
-})
-
-app.get('/hello2', async () => {
-  const transactions = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-  return transactions
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
